2d2363578fbe9a9fd1afa2d79c300177fe5f56bd
Joao Belarmino <joao.silva@webjump.com.br>
2021-08-30 17:17:47 -0300

    footer e politica de privacidade

---
+++ app/code/Webjump/SetupContents/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Copyright © Ulmod. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Webjump_SetupContents',
+    __DIR__
+);
