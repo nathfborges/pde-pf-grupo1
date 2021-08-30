50d9f0ba97a121a351e3fc0122d2069fa86fe033
Mateus Melo <mateus.melo@webjump.com.br>
2021-08-30 10:54:39 -0300

    Testing other ways

--- app/code/Webjump/IBCBackend/Setup/Patch/Data/InstallCatalogRule.php
+++ app/code/Webjump/IBCBackend/Setup/Patch/Data/InstallCatalogRule.php
@@ -5,40 +5,38 @@ namespace Webjump\IBCBackend\Setup\Patch\Data;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\CatalogRule\Api\Data\RuleInterfaceFactory;
-use Magento\CatalogRule\Model\CatalogRuleRepository;
+use Magento\CatalogRule\Api\CatalogRuleRepositoryInterface;
+use Magento\CatalogRule\Api\CatalogRuleRepository;
 use Magento\Customer\Model\Group;
 
 class InstallCatalogRule implements DataPatchInterface
 {
-    /**
-     * @var ModuleDataSetupInterface
-     */
+    /** @var ModuleDataSetupInterface */
     private $moduleDataSetup;
 
-    /**
-     * @var RuleInterfaceFactory
-     */
+    /** @var RuleInterfaceFactory */
     private $ruleFactory;
 
-    /**
-     * @var CatalogRuleRepository
-     */
-    private $catalogRuleRepository;
+    // /**
+    //  * @var CatalogRuleRepositoryInterface
+    //  */
+    // private $catalogRuleRepository;
 
     /**
      * @param ModuleDataSetupInterface $moduleDataSetup
-     * @param Rule $rule
+     * @param RuleInterfaceFactory $ruleFactory
+     * @param CatalogRuleRepositoryInterface $catalogRuleRepository
      */
-    public function __construct(ModuleDataSetupInterface $moduleDataSetup, RuleInterfaceFactory $ruleFactory, CatalogRuleRepository $catalogRuleRepository)
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        RuleInterfaceFactory $ruleFactory,
+        CatalogRuleRepositoryInterface $catalogRuleRepository)
     {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->ruleFactory = $ruleFactory;
-        $this->catalogRuleRepository = $catalogRuleRepository;
+        // $this->catalogRuleRepository = $catalogRuleRepository;
     }
 
-    /**
-     *  {@inheritDoc}
-     */
     public function apply()
     {
         $this->moduleDataSetup->getConnection()->startSetup();
@@ -49,28 +47,28 @@ class InstallCatalogRule implements DataPatchInterface
             ->setName('Discount 5%')
             ->setDescription('5% Discount for Visitors')
             ->setIsActive(1)
-            ->setSortOrder(1)
-            ->setSimpleAction("by_percent")
-            ->setDiscountAmount(5)
             ->setWebsiteIds('1')
             ->setCustomerGroupIds(Group::NOT_LOGGED_IN_ID)
-            ->setStopRulesProcessing(0);
+            ->setSimpleAction("by_percent")
+            ->setDiscountAmount(5)
+            ->setStopRulesProcessing(0)
+            ->save();
 
-        $this->catalogRuleRepository->save($catalogRule5Perc);
+        // $this->catalogRuleRepository->save($catalogRule5Perc);
 
         $catalogRule10Perc = $this->ruleFactory->create();
         $catalogRule10Perc
             ->setName('Discount 10%')
             ->setDescription('10% Discount for Visitors')
             ->setIsActive(1)
-            ->setSortOrder(2)
-            ->setSimpleAction("by_percent")
-            ->setDiscountAmount(10)
             ->setWebsiteIds('2')
             ->setCustomerGroupIds(Group::NOT_LOGGED_IN_ID)
-            ->setStopRulesProcessing(0);
+            ->setSimpleAction("by_percent")
+            ->setDiscountAmount(10)
+            ->setStopRulesProcessing(0)
+            ->save();
 
-        $this->catalogRuleRepository->save($catalogRule10Perc);
+        // $this->catalogRuleRepository->save($catalogRule10Perc);
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
