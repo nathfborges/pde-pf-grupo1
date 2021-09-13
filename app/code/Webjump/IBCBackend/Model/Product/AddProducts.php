6e3d371af8114b11d5f9270787aee849200754bb
Ygor Barcelos <ygor.barcelos@webjump.com.br>
2021-09-13 15:57:00 -0300

    add new stock CSV

--- app/code/Webjump/IBCBackend/Model/Product/AddProducts.php
+++ app/code/Webjump/IBCBackend/Model/Product/AddProducts.php
@@ -49,6 +49,11 @@ class AddProducts
             'entity' => 'catalog_product',
             'behavior' => 'add_update',
             'file' => 'website-productsSkate.csv'
+        ],
+        7 => [
+            'entity' => 'stock_sources',
+            'behavior' => 'append',
+            'file' => 'stockHN.csv'
         ]
     ];
 
